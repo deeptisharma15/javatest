dae5c08dff19a61008364be5a011ead3d813ed6c
deeptisharma15 <deepti1768@gmail.com>
2019-09-26 15:01:28 +0400

    firstproject

---
+++ src/com/emirates/Practice.java
@@ -0,0 +1,18 @@
+package com.emirates;
+
+public class Practice {
+
+    public static void main(String[] args) {
+        System.out.println("I am deepti");
+        System.out.println(addNumbers(10, 5));
+
+
+        for(int i =0; i<10; i++){
+            System.out.println(i);
+        }
+    }
+
+    public static Integer addNumbers(int a, int b){
+        return a + b;
+    }
+}
